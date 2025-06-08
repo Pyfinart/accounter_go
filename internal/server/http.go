1b2d9a2f1fd0ae09c307e806f2c5a61b65afac99
pyfinart <mypyfinart@gmail.com>
2025-06-08 16:43:13 +0800

    feat:accounter:new:var name change

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,34 +1,35 @@
 package server
 
 import (
-	v1 "accounter_go/api/helloworld/v1"
 	accounterv1 "accounter_go/api/accounter/v1"
+	v1 "accounter_go/api/helloworld/v1"
 	"accounter_go/internal/conf"
 	"accounter_go/internal/service"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
-	"github.com/go-kratos/kratos/v2/transport/http"
+	khttp "github.com/go-kratos/kratos/v2/transport/http"
 )
 
 // NewHTTPServer new an HTTP server.
-func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, accounter *service.AccounterService, logger log.Logger) *http.Server {
-	var opts = []http.ServerOption{
-		http.Middleware(
+func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, accounter *service.AccounterService, logger log.Logger) *khttp.Server {
+	var opts = []khttp.ServerOption{
+		khttp.Middleware(
 			recovery.Recovery(),
 		),
 	}
 	if c.Http.Network != "" {
-		opts = append(opts, http.Network(c.Http.Network))
+		opts = append(opts, khttp.Network(c.Http.Network))
 	}
 	if c.Http.Addr != "" {
-		opts = append(opts, http.Address(c.Http.Addr))
+		opts = append(opts, khttp.Address(c.Http.Addr))
 	}
 	if c.Http.Timeout != nil {
-		opts = append(opts, http.Timeout(c.Http.Timeout.AsDuration()))
+		opts = append(opts, khttp.Timeout(c.Http.Timeout.AsDuration()))
 	}
-	srv := http.NewServer(opts...)
+	srv := khttp.NewServer(opts...)
 	v1.RegisterGreeterHTTPServer(srv, greeter)
 	accounterv1.RegisterAccounterHTTPServer(srv, accounter)
+	
 	return srv
 }
