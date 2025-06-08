901fe0c8d81e83093216ae94f0d8c50980ebdacc
pyfinart <mypyfinart@gmail.com>
2025-06-08 16:49:02 +0800

    feat:accounter:new:handle CORS err

--- internal/server/http.go
+++ internal/server/http.go
@@ -9,7 +9,34 @@ import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	khttp "github.com/go-kratos/kratos/v2/transport/http"
+	"net/http"
 )
+
+// CORS中间件
+func corsMiddleware() khttp.FilterFunc {
+	return func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			// 允许的源
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			// 允许的HTTP方法
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			// 允许的请求头
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+			// 允许携带认证信息
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			// 预检请求的缓存时间
+			w.Header().Set("Access-Control-Max-Age", "86400")
+
+			// 处理预检请求
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
+			handler.ServeHTTP(w, r)
+		})
+	}
+}
 
 // NewHTTPServer new an HTTP server.
 func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, accounter *service.AccounterService, logger log.Logger) *khttp.Server {
@@ -17,6 +44,7 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, accounter *s
 		khttp.Middleware(
 			recovery.Recovery(),
 		),
+		khttp.Filter(corsMiddleware()), // 添加CORS中间件
 	}
 	if c.Http.Network != "" {
 		opts = append(opts, khttp.Network(c.Http.Network))
